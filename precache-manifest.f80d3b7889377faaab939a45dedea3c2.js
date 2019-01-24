936c11eb8e53a2bc31060f1ff4969210796c182b
Nicolas Kennof <nicolas.kennof@gmail.com>
2019-01-24 12:56:56 -0600

    Updates

---
+++ precache-manifest.f80d3b7889377faaab939a45dedea3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f42765978d79938d705b68d069586d7",
+    "url": "/16-Clicky-Game/static/media/background.5f427659.jpg"
+  },
+  {
+    "revision": "4194d9dc3607317bac40",
+    "url": "/16-Clicky-Game/static/js/runtime~main.4194d9dc.js"
+  },
+  {
+    "revision": "ef0aaa7751bedfdc0155",
+    "url": "/16-Clicky-Game/static/js/main.ef0aaa77.chunk.js"
+  },
+  {
+    "revision": "5ee2ab859c414a73cce2",
+    "url": "/16-Clicky-Game/static/js/1.5ee2ab85.chunk.js"
+  },
+  {
+    "revision": "ef0aaa7751bedfdc0155",
+    "url": "/16-Clicky-Game/static/css/main.d8ae17b2.chunk.css"
+  },
+  {
+    "revision": "bd3c3838827ca30e35d27c0c6919e375",
+    "url": "/16-Clicky-Game/index.html"
+  }
+];
